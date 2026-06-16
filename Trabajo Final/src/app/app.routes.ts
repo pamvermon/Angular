fb57ed3f7440507a89337e70cbf04e700a9b9450
Pamela Vera <pamvermon@gmail.com>
2026-06-15 22:33:31 -0300

    build: ¡PROYECTO COMPILANDO LOCAL PERFECTO!

--- Trabajo Final/src/app/app.routes.ts
+++ Trabajo Final/src/app/app.routes.ts
@@ -1,15 +1,19 @@
 import { Routes } from '@angular/router';
-import { InicioComponent } from './modules/inicio/inicio';
 
 export const routes: Routes = [
-  { path: '', component: InicioComponent },
-  { 
-    path: 'productos', 
-    loadChildren: () => import('./modules/productos/productos.routes').then(m => m.PRODUCTOS_ROUTES) 
+  {
+    path: 'productos',
+    // Si tenés los componentes creados, los podés cargar directo acá arriba así:
+    // loadComponent: () => import('./modules/productos/productos.component').then(m => m.ProductosComponent)
+    children: [] 
   },
-  { 
-    path: 'usuarios', 
-    loadChildren: () => import('./modules/usuarios/usuarios.routes').then(m => m.USUARIOS_ROUTES) 
+  {
+    path: 'usuarios',
+    children: []
   },
-  { path: '**', redirectTo: '' }
+  {
+    path: '',
+    redirectTo: 'productos',
+    pathMatch: 'full'
+  }
 ];
